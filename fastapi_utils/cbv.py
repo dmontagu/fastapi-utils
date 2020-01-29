f9d42dcc3b6499dc9615dbd340c8e33b72c354d7
David Montague <david@exactrics.com>
2020-01-28 16:33:36 -0800

    Add support for more generic CBVs by way of a generic router

--- fastapi_utils/cbv.py
+++ fastapi_utils/cbv.py
@@ -1,5 +1,8 @@
+import functools
 import inspect
-from typing import Any, Callable, List, Type, TypeVar, Union, get_type_hints
+from copy import deepcopy
+from types import FunctionType
+from typing import Any, Callable, Dict, List, Optional, Type, TypeVar, Union, get_type_hints
 
 from fastapi import APIRouter, Depends
 from pydantic.typing import is_classvar
@@ -8,6 +11,7 @@ from starlette.routing import Route, WebSocketRoute
 T = TypeVar("T")
 
 CBV_CLASS_KEY = "__cbv_class__"
+GENERIC_CBV_ROUTERS_KEY = "__generic_cbv_routers__"
 
 
 def cbv(router: APIRouter) -> Callable[[Type[T]], Type[T]]:
@@ -17,22 +21,48 @@ def cbv(router: APIRouter) -> Callable[[Type[T]], Type[T]]:
     return decorator
 
 
+def generic_cbv(router: APIRouter) -> Callable[[Type[T]], Type[T]]:
+    def decorator(cls: Type[T]) -> Type[T]:
+        generic_routers = getattr(cls, GENERIC_CBV_ROUTERS_KEY, None)
+        if generic_routers is None:
+            generic_routers = []
+            setattr(cls, GENERIC_CBV_ROUTERS_KEY, generic_routers)
+        generic_routers.append(router)
+        return cls
+
+    return decorator
+
+
 def _cbv(router: APIRouter, cls: Type[T]) -> Type[T]:
     _init_cbv(cls)
     cbv_router = APIRouter()
     functions = inspect.getmembers(cls, inspect.isfunction)
-    routes_by_endpoint = {
-        route.endpoint: route for route in router.routes if isinstance(route, (Route, WebSocketRoute))
-    }
+    routes_by_endpoint = _routes_by_endpoint(router)
+    generic_routes_by_endpoint = {}
+    for generic_router in getattr(cls, GENERIC_CBV_ROUTERS_KEY, []):
+        generic_routes_by_endpoint.update(_routes_by_endpoint(generic_router))
     for _, func in functions:
         route = routes_by_endpoint.get(func)
         if route is None:
-            continue
-        router.routes.remove(route)
+            route = generic_routes_by_endpoint.get(func)
+            if route is None:
+                continue
+        else:
+            router.routes.remove(route)
+        route = deepcopy(route)
+        route.endpoint = replace_method_with_copy(cls, func)
         _update_cbv_route_endpoint_signature(cls, route)
         cbv_router.routes.append(route)
     router.include_router(cbv_router)
     return cls
+
+
+def _routes_by_endpoint(router: Optional[APIRouter]) -> Dict[Callable[..., Any], Union[Route, WebSocketRoute]]:
+    return (
+        {}
+        if router is None
+        else {route.endpoint: route for route in router.routes if isinstance(route, (Route, WebSocketRoute))}
+    )
 
 
 def _update_cbv_route_endpoint_signature(cls: Type[Any], route: Union[Route, WebSocketRoute]) -> None:
@@ -78,3 +108,18 @@ def _init_cbv(cls: Type[Any]) -> None:
     setattr(cls, "__signature__", new_signature)
     setattr(cls, "__init__", new_init)
     setattr(cls, CBV_CLASS_KEY, True)
+
+
+def replace_method_with_copy(cls: Type[Any], function: FunctionType) -> FunctionType:
+    copied = FunctionType(
+        function.__code__,
+        function.__globals__,
+        name=function.__name__,
+        argdefs=function.__defaults__,
+        closure=function.__closure__,
+    )
+    functools.update_wrapper(copied, function)
+    copied.__qualname__ = f"{cls.__name__}.{function.__name__}"
+    copied.__kwdefaults__ = function.__kwdefaults__
+    setattr(cls, function.__name__, copied)
+    return copied
