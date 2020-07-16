2bb90c190cc3632dae5225d1adae147a0d26ce5f
Yuval Levi <yuvi9313@gmail.com>
2020-07-16 17:23:00 +0300

    Lint tests

--- tests/test_cbv_base.py
+++ tests/test_cbv_base.py
@@ -1,15 +1,11 @@
-from typing import Any, ClassVar
-
-from fastapi import APIRouter, Depends, FastAPI
+from fastapi import FastAPI
 from starlette.testclient import TestClient
 
-from fastapi_utils.cbv_base import Resource, Api, set_responses
+from fastapi_utils.cbv_base import Api, Resource, set_responses
 
 
 def test_cbv() -> None:
-
     class CBV(Resource):
-
         def __init__(self, z: int = 1):
             super().__init__()
             self.y = 1
@@ -27,10 +23,10 @@ def test_cbv() -> None:
     app = FastAPI()
     api = Api(app)
     cbv = CBV(2)
-    api.add_resource(cbv, '/', '/classvar')
+    api.add_resource(cbv, "/", "/classvar")
 
     client = TestClient(app)
-    response_1 = client.post("/", params={'x': 1}, json={})
+    response_1 = client.post("/", params={"x": 1}, json={})
     assert response_1.status_code == 200
     assert response_1.content == b"4"
 
@@ -40,9 +36,7 @@ def test_cbv() -> None:
 
 
 def test_arg_in_path() -> None:
-
     class TestCBV(Resource):
-
         @set_responses(str)
         def get(self, item_id: str):
             return item_id
@@ -51,14 +45,13 @@ def test_arg_in_path() -> None:
     api = Api(app)
 
     test_cbv_resource = TestCBV()
-    api.add_resource(test_cbv_resource, '/{item_id}')
+    api.add_resource(test_cbv_resource, "/{item_id}")
 
-    assert TestClient(app).get("/test").json() == 'test'
+    assert TestClient(app).get("/test").json() == "test"
 
 
 def test_multiple_routes() -> None:
     class RootHandler(Resource):
-
         def get(self, item_path: str = None):
             if item_path:
                 return {"item_path": item_path}
@@ -68,7 +61,7 @@ def test_multiple_routes() -> None:
     api = Api(app)
 
     root_handler_resource = RootHandler()
-    api.add_resource(root_handler_resource, '/items/?', '/items/{item_path:path}')
+    api.add_resource(root_handler_resource, "/items/?", "/items/{item_path:path}")
 
     client = TestClient(app)
 
