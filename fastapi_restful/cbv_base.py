883503433bcfd456c2515c06b9a20b94a3b73bd3
Yuval Levi <yuvi9313@gmail.com>
2020-07-17 15:53:10 +0300

    Correct imports

--- fastapi_restful/cbv_base.py
+++ fastapi_restful/cbv_base.py
@@ -1,7 +1,7 @@
 from typing import Any, Dict, Optional, Tuple
 
 from fastapi import APIRouter, FastAPI
-from fastapi_utils.cbv import INCLUDE_INIT_PARAMS_KEY, RETURN_TYPES_FUNC_KEY, _cbv
+from .cbv import INCLUDE_INIT_PARAMS_KEY, RETURN_TYPES_FUNC_KEY, _cbv
 
 
 class Resource:
