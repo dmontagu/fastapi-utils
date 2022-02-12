74ff82dae843a236975daac143f9d0d0d10aa735
Yuval Levi <yuvall9313@gmail.com>
2022-02-12 18:03:55 +0200

    Update tests to fit better practices

--- tests/test_cbv.py
+++ tests/test_cbv.py
@@ -1,99 +1,130 @@
 from typing import Any, ClassVar
 
-from fastapi import APIRouter, Depends, FastAPI
+import pytest
+from fastapi import APIRouter, Depends
 from starlette.testclient import TestClient
 
 from fastapi_restful.cbv import cbv
 
 
-def test_cbv() -> None:
-    router = APIRouter()
+class TestCBV:
+    @pytest.fixture(autouse=True)
+    def router(self) -> APIRouter:
+        return APIRouter()
 
-    def dependency() -> int:
-        return 1
+    def test_response_models(self, router: APIRouter) -> None:
+        expected_response = "home"
 
-    @cbv(router)
-    class CBV:
-        x: int = Depends(dependency)
-        cx: ClassVar[int] = 1
-        cy: ClassVar[int]
+        @cbv(router)
+        class CBV:
+            def __init__(self) -> None:
+                self.one = 1
+                self.two = 2
 
-        def __init__(self, z: int = Depends(dependency)):
-            self.y = 1
-            self.z = z
+            @router.get("/", response_model=str)
+            def string_response(self) -> str:
+                return expected_response
 
-        @router.get("/", response_model=int)
-        def f(self) -> int:
-            return self.cx + self.x + self.y + self.z
+            @router.get("/sum", response_model=int)
+            def int_response(self) -> int:
+                return self.one + self.two
 
-        @router.get("/classvar", response_model=bool)
-        def g(self) -> bool:
-            return hasattr(self, "cy")
+        client = TestClient(router)
+        response_1 = client.get("/")
+        assert response_1.status_code == 200
+        assert response_1.json() == expected_response
 
-    app = FastAPI()
-    app.include_router(router)
-    client = TestClient(app)
-    response_1 = client.get("/")
-    assert response_1.status_code == 200
-    assert response_1.content == b"4"
+        response_2 = client.get("/sum")
+        assert response_2.status_code == 200
+        assert response_2.content == b"3"
 
-    response_2 = client.get("/classvar")
-    assert response_2.status_code == 200
-    assert response_2.content == b"false"
-
-
-def test_method_order_preserved() -> None:
-    router = APIRouter()
-
-    @cbv(router)
-    class TestCBV:
-        @router.get("/test")
-        def get_test(self) -> int:
+    def test_dependencies(self, router: APIRouter) -> None:
+        def dependency_one() -> int:
             return 1
 
-        @router.get("/{item_id}")
-        def get_item(self) -> int:  # Alphabetically before `get_test`
+        def dependency_two() -> int:
             return 2
 
-    app = FastAPI()
-    app.include_router(router)
+        @cbv(router)
+        class CBV:
+            one: int = Depends(dependency_one)
 
-    assert TestClient(app).get("/test").json() == 1
-    assert TestClient(app).get("/other").json() == 2
+            def __init__(self, two: int = Depends(dependency_two)):
+                self.two = two
 
+            @router.get("/", response_model=int)
+            def int_dependencies(self) -> int:
+                return self.one + self.two
 
-def test_multiple_decorators() -> None:
-    router = APIRouter()
+        client = TestClient(router)
+        response = client.get("/")
+        assert response.status_code == 200
+        assert response.content == b"3"
 
-    @cbv(router)
-    class RootHandler:
-        @router.get("/items/?")
-        @router.get("/items/{item_path:path}")
-        @router.get("/database/{item_path:path}")
-        def root(self, item_path: str = None, item_query: str = None) -> Any:
-            if item_path:
-                return {"item_path": item_path}
-            if item_query:
-                return {"item_query": item_query}
-            return []
+    def test_class_var(self, router: APIRouter) -> None:
+        @cbv(router)
+        class CBV:
+            class_var: ClassVar[int]
 
-    client = TestClient(router)
+            @router.get("/", response_model=bool)
+            def g(self) -> bool:
+                return hasattr(self, "class_var")
 
-    assert client.get("/items").json() == []
-    assert client.get("/items/1").json() == {"item_path": "1"}
-    assert client.get("/database/abc").json() == {"item_path": "abc"}
+        client = TestClient(router)
+        response = client.get("/")
+        assert response.status_code == 200
+        assert response.content == b"false"
 
+    def test_routes_path_order_preserved(self, router: APIRouter) -> None:
+        @cbv(router)
+        class CBV:
+            @router.get("/test")
+            def get_test(self) -> int:
+                return 1
 
-def test_prefix() -> None:
-    router = APIRouter(prefix="/api")
+            @router.get("/{any_path}")
+            def get_any_path(self) -> int:  # Alphabetically before `get_test`
+                return 2
 
-    @cbv(router)
-    class RootHandler:
-        @router.get("/item")
-        def root(self) -> str:
-            return "hello"
+        client = TestClient(router)
+        assert client.get("/test").json() == 1
+        assert client.get("/any_other_path").json() == 2
 
-    client = TestClient(router)
-    response = client.get("/api/item")
-    assert response.status_code == 200
-    assert response.json() == "hello"
+    def test_multiple_paths(self, router: APIRouter) -> None:
+        @cbv(router)
+        class CBV:
+            @router.get("/items")
+            @router.get("/items/{custom_path:path}")
+            @router.get("/database/{custom_path:path}")
+            def root(self, custom_path: str = None) -> Any:
+                return {"custom_path": custom_path} if custom_path else []
+
+        client = TestClient(router)
+        assert client.get("/items").json() == []
+        assert client.get("/items/1").json() == {"custom_path": "1"}
+        assert client.get("/database/abc").json() == {"custom_path": "abc"}
+
+    def test_query_parameters(self, router: APIRouter) -> None:
+        @cbv(router)
+        class CBV:
+            @router.get("/route")
+            def root(self, param: int = None) -> int:
+                return param if param else 0
+
+        client = TestClient(router)
+        assert client.get("/route").json() == 0
+        assert client.get("/route?param=3").json() == 3
+
+    def test_prefix(self) -> None:
+        router = APIRouter(prefix="/api")
+
+        @cbv(router)
+        class CBV:
+            @router.get("/item")
+            def root(self) -> str:
+                return "hello"
+
+        client = TestClient(router)
+        response = client.get("/api/item")
+        assert response.status_code == 200
+        assert response.json() == "hello"
