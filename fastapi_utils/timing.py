0d1418379ab66fd436882fe6f1f15e20e1dee78c
David Viana <me@davidviana.com>
2023-05-13 15:07:18 +0100

    Add _get_cpu_time win32 specific implementation

--- fastapi_utils/timing.py
+++ fastapi_utils/timing.py
@@ -186,6 +186,9 @@ def _get_cpu_time() -> float:
     """
     Generates the cpu time to report. Adds the user and system time, following the implementation from timing-asgi
     """
+    if sys.platform == "win32":
+        return time.process_time()
+
     resources = resource.getrusage(resource.RUSAGE_SELF)
     # add up user time (ru_utime) and system time (ru_stime)
     return resources[0] + resources[1]
