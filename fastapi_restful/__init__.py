aceb17806b8245c64e9bcd69e3b27d1652c7ca27
Pavel Zwerschke <pavelzw@gmail.com>
2023-08-13 20:04:40 +0200

    Add proper __version__ (#214)
    * Add proper __version__
    
    * Conditional __version__

--- fastapi_restful/__init__.py
+++ fastapi_restful/__init__.py
@@ -1,6 +1,27 @@
+import sys
+import warnings
+
 from .cbv_base import Api, Resource, set_responses, take_init_parameters
 
-__version__ = "0.4.3"
+if sys.version_info.minor < 8:
+    import pkg_resources
+
+    try:
+        __version__ = pkg_resources.get_distribution(__name__).version
+    except pkg_resources.DistributionNotFound as e:
+        warnings.warn(f"Could not determine version of {__name__}", stacklevel=1)
+        warnings.warn(str(e), stacklevel=1)
+        __version__ = "unknown"
+else:
+    import importlib.metadata
+
+    try:
+        __version__ = importlib.metadata.version(__name__)
+    except importlib.metadata.PackageNotFoundError as e:
+        warnings.warn(f"Could not determine version of {__name__}", stacklevel=1)
+        warnings.warn(str(e), stacklevel=1)
+        __version__ = "unknown"
+
 
 __all__ = [
     "Api",
